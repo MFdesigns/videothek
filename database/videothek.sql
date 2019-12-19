c0153b19918e9616e935809df6d642d295cca390
Dario Romandini <darioromandini@Darios-MacBook-Air.local>
2019-12-19 13:27:56 +0100

    added database script

---
+++ database/videothek.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE videothek;
+
+USE videothek;
+
+CREATE TABLE TVideos (
+	VidNumber INT NOT NULL,
+    VidTitle VARCHAR(64),
+    VidDuration TIME,
+    VidCategory VARCHAR(64),
+    VidYear DATE,
+    VidAgeRating INT,
+    VidPricePerDay FLOAT,
+    VidPrice FLOAT,
+    VidInventory INT,
+    PRIMARY KEY (VidNumber)
+);
+
+CREATE TABLE TPlaces(
+	PlaceONRP INT NOT NULL,
+    PlaceStreet INT,
+    PlaceNumber INT,
+    PlaceCity VARCHAR(64),
+    PRIMARY KEY (PlaceONRP)
+);
+
+CREATE TABLE TCustomers (
+	CustId INT NOT NULL,
+    CustTitle ENUM('Herr', 'Frau'),
+    CustName VARCHAR(128),
+    CustSurname VARCHAR(128),
+    CustBirthday DATE,
+    CustPhoneNumber INT,
+    PlaceONRP INT,
+    PRIMARY KEY (CustId),
+    FOREIGN KEY (PlaceONRP) REFERENCES TPlaces (PlaceONRP)
+);
+
+CREATE TABLE TLendings (
+	LendId INT NOT NULL,
+    LendFrom DATE,
+    LendUntill DATE,
+    VidNumber INT,
+    CustId INT,
+    PRIMARY KEY (LendId),
+    FOREIGN KEY (CustId) REFERENCES TCustomers (CustId),
+    FOREIGN KEY (VidNumber) REFERENCES TVideos (VidNumber)
+);
