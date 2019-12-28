52400c5401f40deaab59710b0a2dc23151497f0d
MFdesigns <michel.faeh@stafag.ch>
2019-12-28 19:23:08 +0100

    Added basic layout and fixed font

--- src/views/CustomerView.php
+++ src/views/CustomerView.php
@@ -10,38 +10,60 @@
 
   <!-- Header -->
   <header>
-    <h1 class="page-title">Videothek</h1>
-    <!-- Page navigation -->
-    <nav class="nav">
-      <ul class="nav-list">
-        <li>
-          <a class="nav-list__item nav-list__item--selected text-sm--light" href="/kunden">
-            <img class="nav-list__item-icon" src="/res/icons/placeholder_icon.svg"/>
-            <span class="nav-list__item-text">Kunden</span>
-          </a>
-        </li>
-        <li>
-          <a class="nav-list__item text-sm--light" href="#">
-            <img class="nav-list__item-icon" src="/res/icons/placeholder_icon.svg"/>
-            <span class="nav-list__item-text">Videos</span>
-          </a>
-        </li>
-        <li>
-          <a class="nav-list__item text-sm--light" href="#">
-            <img class="nav-list__item-icon"  src="/res/icons/placeholder_icon.svg"/>
-            <span class="nav-list__item-text">Ausleihen</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div class="header-container">
+      <h1 class="page-title title--light">Videothek</h1>
+      <!-- Page navigation -->
+      <nav class="nav">
+        <ul class="nav-list">
+          <li>
+            <a class="nav-list__item nav-list__item--selected text-sm--light" href="/kunden">
+              <img class="nav-list__item-icon" src="/res/icons/placeholder_icon.svg"/>
+              <span class="nav-list__item-text">Kunden</span>
+            </a>
+          </li>
+          <li>
+            <a class="nav-list__item text-sm--light" href="#">
+              <img class="nav-list__item-icon" src="/res/icons/placeholder_icon.svg"/>
+              <span class="nav-list__item-text">Videos</span>
+            </a>
+          </li>
+          <li>
+            <a class="nav-list__item text-sm--light" href="#">
+              <img class="nav-list__item-icon"  src="/res/icons/placeholder_icon.svg"/>
+              <span class="nav-list__item-text">Ausleihen</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 
   <main>
-    <h2>Kunden</h2>
+    <div class="section page-subheader">
+      <h2 class="title-lg--accent">Kunden</h2>
+    </div>
+
+    <div class="section customer-list">
+      <h3 class="card-title title--dark">Liste</h3>
+      <div class="card"></div>
+      <!-- Customer list content -->
+    </div>
+
+    <div class="section customer-info">
+      <h3 class="card-title title--dark">Infos</h3>
+      <div class="card"></div>
+      <!-- Customer lendings content -->
+    </div>
+
+    <div class="section customer-lendings">
+      <h3 class="card-title title--dark">Ausleihen</h3>
+      <div class="card"></div>
+      <!-- Customer lendings content -->
+    </div>
   </main>
 
   <footer>
-    <p>Footer</p>
+    <p>Copyright © Webdesign AG</p>
   </footer>
 
 </body>
