78a2e1f61608fbf2fd8b547444a120f2ce80241b
MFdesigns <michel.faeh@stafag.ch>
2019-12-30 20:53:29 +0100

    Added panic function

--- src/utils.php
+++ src/utils.php
@@ -1,8 +1,24 @@
 <?php
 
+/**
+ * Returns 404 status and displays custom 404 page
+ *
+ * @return void
+ */
 function pageNotFound() {
   http_response_code(404);
   // TODO: custom 404 page
+  die();
+}
+
+/**
+ * Sends HTTP response code and exits programm
+ *
+ * @param int $responseCode
+ * @return void
+ */
+function panic($responseCode) {
+  http_response_code($responseCode);
   die();
 }
 
